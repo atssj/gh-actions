a2c94670d1c5adce03d71295bbd3da611117573d
Subhajit Saha <103457002+atssj@users.noreply.github.com>
2025-03-17 14:25:03 +0530

    chore(core): update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  helpUrl: "https://github.com/atssj/gh-actions/blob/main/CONTRIBUTING.md",
   rules: {
     "scope-enum": [
       2,
@@ -10,15 +11,20 @@ module.exports = {
       2,
       "always",
       [
-        "feat", // New features
-        "fix", // Bug fixes
-        "docs", // Documentation changes
-        "style", // Code style changes
+        "feat",     // New features
+        "fix",      // Bug fixes
+        "docs",     // Documentation changes
+        "style",    // Code style changes
         "refactor", // Code refactoring
-        "test", // Adding/updating tests
-        "chore", // Maintenance tasks
-        "ci", // CI/CD changes
+        "test",     // Adding/updating tests
+        "chore",    // Maintenance tasks
+        "ci",       // CI/CD changes
       ],
     ],
+    "subject-case": [
+      2,
+      "never",
+      ["start-case", "pascal-case", "upper-case"]
+    ]
   },
 };
